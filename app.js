c6e4f1f8fa1a79dc2f57a8a247492b24db4f3fb8
Zoe Stephens <zoe01stephens@gmail.com>
2022-11-07 18:44:17 -0500

    Edited JavaScript file

--- app.js
+++ app.js
@@ -4,29 +4,50 @@ window.onload = function(){
     var httpRequest
     var link = "http://localhost/info2180-lab4/superheroes.php"
 
+    var textField = document.querySelector("input") 
+    var resultBox = document.querySelector("#result")
+
     searchBtn.addEventListener("click", function(e){
         e.preventDefault()
 
-        
         httpRequest = new XMLHttpRequest()
+
+        httpRequest.onreadystatechange = function(){
+            if (httpRequest.readyState == 4){
+                if (httpRequest.status === 200){
+                    
+                    var response = httpRequest.responseText
+                    // alert(response)
+                    resultBox.innerHTML = response
+                } else{
+                    //alert(":( Something went wrong...")
+                    resultBox.innerHTML = "Something went wrong :("
+                }
+            }
+
+            
+        }
+        httpRequest.open("GET", link + "?query=" + textField.value)
+            httpRequest.send()
+
 
         //GET Request
 
-        httpRequest.onreadystatechange = loadHero
-        httpRequest.open("GET", link)
-        httpRequest.send()
+        // httpRequest.onreadystatechange = loadHero
+        // httpRequest.open("GET", link)
+        // httpRequest.send()
     })
 
-    function loadHero(){
-        if (httpRequest.readyState === XMLHttpRequest.DONE){
-            if (httpRequest.status === 200){
+    // function loadHero(){
+    //     if (httpRequest.readyState === XMLHttpRequest.DONE){
+    //         if (httpRequest.status === 200){
                 
-                var response = httpRequest.responseText
-                alert(response)
-            } else{
-                alert(":( Something went wrong...")
-            }
-        }
-    }
+    //             var response = httpRequest.responseText
+    //             alert(response)
+    //         } else{
+    //             alert(":( Something went wrong...")
+    //         }
+    //     }
+    // }
 
 }
